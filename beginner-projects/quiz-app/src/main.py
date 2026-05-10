2b70bfa15e73d6b42bf4fe595bcc7c53b2d0e29d
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-05-10 20:31:11 +0545

    Add timed quiz input and random question shuffling

--- beginner-projects/quiz-app/src/main.py
+++ beginner-projects/quiz-app/src/main.py
@@ -1,7 +1,15 @@
 import json
+import signal
+import random
 from json import JSONDecodeError
 
 FILE = "data/questions.json"
+
+class TimeoutException(Exception):
+    pass
+
+def handler(signum, frame):
+    raise TimeoutException
 
 def load_questions():
     try:
@@ -12,6 +20,7 @@ def load_questions():
     
 def main():
     questions = load_questions()
+    random.shuffle(questions)
     score = 0
     print("::::: Quiz Application :::::")
     print("Choose option a, b, c or d.")
@@ -20,9 +29,16 @@ def main():
         options = q["options"]
         for key, value in options.items():
             print(f"{key}. {value}")
-        answer = input("Answer: ")[0]
-        if q["answer"].lower() == answer.lower():
-            score += 1
+
+        signal.signal(signal.SIGALRM, handler)
+        signal.alarm(5)
+        print("You have 5 seconds to answer.")
+        try:
+            answer = input("Answer: ")[0]
+            if q["answer"].lower() == answer.lower():
+                score += 1
+        except TimeoutException:
+            print("Time's Up")
 
     print(f"You scored {score}/{len(questions)}")
 
