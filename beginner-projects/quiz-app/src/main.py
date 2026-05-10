80be724faaad63cb2b6a95cb58a6ff66b1afb248
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-05-10 21:14:50 +0545

    feat: add timed quiz, question shuffling, and leaderboard system

--- beginner-projects/quiz-app/src/main.py
+++ beginner-projects/quiz-app/src/main.py
@@ -4,6 +4,7 @@ import random
 from json import JSONDecodeError
 
 FILE = "data/questions.json"
+LEADERBOARD_FILE = "data/leaderboard.json"
 
 class TimeoutException(Exception):
     pass
@@ -15,14 +16,47 @@ def load_questions():
     try:
         with open(FILE, "r") as f:
             return json.load(f)
-    except JSONDecodeError:
+    except (JSONDecodeError, FileNotFoundError):
         return []
     
+def save_score(name, score, total):
+    try:
+        with open(LEADERBOARD_FILE, "r") as f:
+            data = json.load(f)
+    except (JSONDecodeError, FileNotFoundError):
+        data = []
+
+    data.append({
+        "name" : name,
+        "score" : score,
+        "total" : total
+    })
+
+    with open(LEADERBOARD_FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+def show_leaderboard():
+    try:
+        with open(LEADERBOARD_FILE, "r") as f:
+            entry = json.load(f)
+        
+        entry.sort(key=lambda x:x["score"], reverse=True)
+
+        print("===== Leaderboard =====")
+        for i, entry in enumerate(entry, start=1):
+            print(f"{i}. {entry['name']}: {entry['score']}/{entry['total']}")
+            
+    except (JSONDecodeError, FileNotFoundError):
+        print("No leaderboard data yet")
+
+    
+
 def main():
     questions = load_questions()
     random.shuffle(questions)
     score = 0
     print("::::: Quiz Application :::::")
+    name = input("Enter your name:")
     print("Choose option a, b, c or d.")
     for i, q in enumerate(questions, start=1):
         print(f"{i}. {q["question"]}")
@@ -39,8 +73,11 @@ def main():
                 score += 1
         except TimeoutException:
             print("Time's Up")
-
-    print(f"You scored {score}/{len(questions)}")
+    total = len(questions)
+    print(f"You scored {score}/{total}")
+    save_score(name, score, total)
+    show_leaderboard()
+    
 
 main()
         
