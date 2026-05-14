0f7a9d975f3394c38d11fc94eed75d94362f068c
Saurav Tamrakar <tamrakarsurav101@gmail.com>
2026-05-14 12:31:01 +0545

    feat(file backup tool): add hash based file copy and JSON metadata file

--- script-files/backup.py
+++ script-files/backup.py
@@ -1,20 +1,45 @@
 import os
 import sys
 import shutil
+import json
+import hashlib
 from datetime import datetime
+from json import JSONDecodeError
 
 IGNORE_DIRS = ["venv", ".git", "__pycache__"]
+METADATA_FILE = "metadata.json"
+
 log = [] 
 
 def append_log(message):
     log.append(message + "\n")
 
+def load_metadata():
+    try:
+        with open(METADATA_FILE, 'r') as f:
+            return json.load(f)
+    except (JSONDecodeError, FileNotFoundError):
+        return {}
+    
+def save_metadata(data):
+    with open(METADATA_FILE, 'w') as f:
+        json.dump(data, f, indent=4)
+    print("Metadata saved successfully.")
+
+def get_file_hash(file_path):
+    hasher = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        while chunk := f.read(4096):
+            hasher.update(chunk)
+        
+    return hasher.hexdigest()   
+
 def should_copy(src, dest):
     if not os.path.exists(dest):
         return True
-    return os.path.getmtime(src) > os.path.getmtime(dest)
+    return get_file_hash(src) != get_file_hash(dest)
 
-def copy_files(source, backup):
+def copy_files(source, backup, metadata):
     for root, dirs, filenames in os.walk(source):
         dirs[:] = [d for d in dirs if d not in IGNORE_DIRS]
 
@@ -30,6 +55,11 @@ def copy_files(source, backup):
                 if should_copy(src, dest):
                     shutil.copy2(src, dest)
                     append_log(f"{datetime.now()} ::: COPIED ::: {src}")
+                    metadata[os.path.relpath(src, source)] = {
+                        "hash": get_file_hash(src),
+                        "last_modified": os.path.getmtime(src),
+                        "backup_path": dest
+                    }
                 else:
                     append_log(f"{datetime.now()} ::: SKIPPED ::: {src}")
 
@@ -45,6 +75,7 @@ def main():
         print("Usage: python backup.py <source> <backup>")
         return
 
+    metadata = load_metadata()
     source = os.path.abspath(sys.argv[1])
     backup = os.path.abspath(sys.argv[2])
 
@@ -56,11 +87,15 @@ def main():
         print("Filepath doesn't exist")
         return
 
-    copy_files(source, backup)
+    copy_files(source, backup, metadata)
     save_log()
-
+    save_metadata(metadata)
+    
     print("Backup completed successfully.")
     print("Log updated.")
 
 if __name__ == "__main__":
     main()
+
+
+
